f9986438ebc160938f49f90bc698234e2aacefd4
minaamfasihi <minaam.fasihi@gmail.com>
2017-12-07 19:58:22 +0500

    Initial commit

---
+++ WindowsFormsTutorial/MessageBoxHelloWorld.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsTutorial
+{
+    class MessageBoxHelloWorld
+    {
+        public void PlayingWithWindowsForms()
+        {
+            //MessageBox.Show("Hello World");
+            //MessageBox.Show("Hello, World!", "MessageBoxHelloWorld");
+            //MessageBox.Show("Hi there", "MessageBoxHelloWorld", MessageBoxButtons.OKCancel);
+            //MessageBox.Show("Hi there", "MessageBoxHelloWorld", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            DialogResult dr = MessageBox.Show("Hi there", "MessageBoxHelloWorld", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2);
+            switch (dr)
+            {
+                case DialogResult.OK:
+                    Console.WriteLine("Yoohooo");
+                    break;
+                case DialogResult.Cancel:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //static void Main(string[] args)
+        //{
+            
+        //}
+    }
+}
---
+++ WindowsFormsTutorial/MyDocumentsFolder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsTutorial
+{
+    class MyDocumentsFolder
+    {
+        public static void Main()
+        {
+            MessageBox.Show(
+                Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                "My Documents Folder"
+                );
+        }
+    }
+}
